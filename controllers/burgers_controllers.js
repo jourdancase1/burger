4a6b437ec4262e8598440938974d68178dfcf2a6
user <user@Jourdans-iMac.lan>
2020-09-10 10:39:43 -0500

    updated sql and controller files

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -4,8 +4,37 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
+router.get("/burgers", function(req, res){
+    burgers.selectAll(function(req, res){
+        var hbsObject = {
+            bugers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject)
+    });
+});
 
+// create new burger
+router.post("/bugers/create", function(req, res){
+    burgers.insertOne([
+        "burger_name"
+    ],[
+        req.body.burger_name
+    ], function(data){
+        res.redirect("/burgers");
+    })
+})
 
+router.put("/burgers/update/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    burgers.updateOne({
+        "devoured": req.body.devoured
+    }, condition, funciton(data){
+        res.redirect("/burgers")
+    })
+})
 
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,4 @@
 -- populate burger db with at least three entries
+INSERT INTO burgers (burger_name) VALUES ("bacon");
+INSERT INTO burgers (burger_name) VALUES ("cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("jalapeno");
